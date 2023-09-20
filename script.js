5842bba927c8be7eb114daea42ab79b4e1908dd7
dmitri-coder <zverkov.dmitri@gmail.com>
2023-09-20 14:47:19 +0300

    add  some functions

--- script.js
+++ script.js
@@ -1,70 +1,41 @@
-function hello(name='Nimetu', age=Infinity){
-    let yearText = 'years';
-    if(age === 1){
-        yearText = 'year';
-    }
-    //console.log('hello ' + name + ' and you are ' + age + ' years old');
-    console.log(`hello ${name} and you are ${age} ${yearText} old`);
+function hello(){
+    console.log('hello1');
+}
+let hello2 = function (){
+    console.log('hello2');
 }
 
-hello('Karin Eegreid', 69);
-hello('Ylle Doos', 42);
-hello('Imre Tard', 1);
+let hello3 = () => {
+    console.log('hello3');
+}
+
+let hello4 = () => console.log('hello4');
+
 hello();
+hello2();
+hello3();
+hello4();
 
-function volume(h, l, w){
-    return h*w*l;
-}
-
-let answer = volume(4,2,5);
-console.log(answer);
-
-function numbers(i) {
-    console.log(i);
-    if(i<10){
-        numbers(i+1);
+let person = {
+    name: 'Karin',
+    age: '1',
+    describe(){
+        console.log(this);
+        console.log('name:' + this.name + ' age:' + this.name);
+    },
+    describe2: () => {
+        console.log(this);
+        console.log('name:' + this.name + ' age:' + this.name);
+    },
+    describe3: function () {
+        console.log(this);
+        console.log('name:' + this.name + ' age:' + this.name);
     }
 }
-numbers(0);
 
-let nums = [0, 1, 2, 3, 4,];
+person.name = 'martin';
+person.age = '33';
 
-function print(num){
-    console.log(num);
-}
-
-nums.forEach(print);
-nums.forEach(function (num){
-    console.log('Num is ' + num);
-});
-
-nums.forEach(num => {
-    console.log('Num is ' + num);
-});
-
-nums.forEach(num =>console.log('Num is ' + num));
-
-let  sums = nums.map(num => {
-    return num +10;
-});
-console.log(sums);
-
-//sums = nums.map(num => num + 10);
-//console.log(sums);
-
-//let sum = nums.reduce((total, num) => total+num);
-//console.log(sums);
-
-//let sorted = num.sort((a,b) => {
- //   if(a > b) {
- //       return 1;
- //   } else if(a < b ) {
- //       return -1;
- //   } else {
- //       return 0;
-  //  }
-//});
-//console.log(sorted);
-
-console.log(Math.PI);
-console.log(Math.round(Math.random() *50 ) + 50);
+person.describe();
+person.describe2();
+person.describe3();
