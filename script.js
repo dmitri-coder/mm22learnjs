792cff57f166fbcb1baedc474ac6bdfac6ebe006
dmitri-coder <zverkov.dmitri@gmail.com>
2023-09-25 11:12:53 +0300

    add paint/game that dont work

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-ctx.fillStyle = 'blue';
-ctx.fillRect(10, 1, 100, 30);
 
-ctx.fillStyle = 'black';
-ctx.fillRect(10, 30, 100, 30);
+let x = 10;
+let y = 10;
 
-ctx.rect(10, 0, 100, 90);
-ctx.strokeStyle = 'black';
-ctx.lineWidth = 1;
-ctx.stroke();
+setInterval(() => {
+    if(pressed){
+        ctx.beginPath();
+        ctx.reset();
+        ctx.arc(x, y, 30, 0, 2*Math.PI);
+        ctx.fill();
+    }
+}, 16);
 
-ctx.moveTo(150,20);
-ctx.lineTo(140,50);
-ctx.stroke();
-ctx.beginPath();
-
-ctx.moveTo(150,20);
-ctx.lineTo(160,50);
-ctx.stroke();
-ctx.beginPath();
-
-ctx.moveTo(130,30);
-ctx.lineTo(160,50);
-ctx.stroke();
-ctx.beginPath(); 
-
-ctx.moveTo(140,50);
-ctx.lineTo(170,30);
-ctx.stroke();
-ctx.beginPath(); 
-
-ctx.moveTo(130,30);
-ctx.lineTo(170,30);
-ctx.stroke();
-ctx.beginPath(); 
-
-ctx.moveTo(0,0);
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(200,100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'black';
-ctx.lineWidth = 1;
-ctx.stroke();
-
-ctx.beginPath();
-ctx.lineTo(200,100);
-ctx.arc(200, 100, 50, 155, -201 / Math.PI);
-ctx.fillStyle = 'black';
-ctx.fill();
-ctx.strokeStyle = 'black';
-ctx.stroke();
+canvas.addEventListener('mousemove', event => {
+    console.log(event);
+    x = event.clientX - canvas.offsetLeft;
+    y = event.clientY - canvas.offsetTop;
+});
+canvas.addEventListener('mousdown', event => {
+   pressed = true;
+});
+canvas.addEventListener('mouseup', event => {
+    pressed = false;
+ });
+ let size = document.querySelector('#size');
+ let sizeValue = document.querySelector('#sizevalue')
+ size.addEventListener('input', ()=> {
+    sizeValue.innerHTML = size.value;
+ })
