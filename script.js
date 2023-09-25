005012bcd051d5803015437d7e19167dad441753
dmitri-coder <zverkov.dmitri@gmail.com>
2023-09-25 10:08:46 +0300

    add est flag star and pie chart

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
+ctx.fillStyle = 'blue';
+ctx.fillRect(10, 1, 100, 30);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(10, 30, 100, 30);
+
+ctx.rect(10, 0, 100, 90);
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+ctx.moveTo(150,20);
+ctx.lineTo(140,50);
 ctx.stroke();
 ctx.beginPath();
-ctx.arc(100,100, 50, 0, 2 * Math.PI);
-ctx.strokeStyle = 'red'
-ctx.lineWidth = 3;
+
+ctx.moveTo(150,20);
+ctx.lineTo(160,50);
 ctx.stroke();
-ctx.font = "30px Arial";
-ctx.fillText('Karin Eegreid', 30, 50);
-ctx.strokeText('Imre Tard', 30, 100);
-ctx.fillStyle = '';
-ctx.fillRect(10, 10, 10, 10);
+ctx.beginPath();
+
+ctx.moveTo(130,30);
+ctx.lineTo(160,50);
+ctx.stroke();
+ctx.beginPath(); 
+
+ctx.moveTo(140,50);
+ctx.lineTo(170,30);
+ctx.stroke();
+ctx.beginPath(); 
+
+ctx.moveTo(130,30);
+ctx.lineTo(170,30);
+ctx.stroke();
+ctx.beginPath(); 
+
+ctx.moveTo(0,0);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(200,100, 50, 0, 2 * Math.PI);
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 1;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.lineTo(200,100);
+ctx.arc(200, 100, 50, 30, 2 * Math.PI);
+ctx.fillStyle = 'black';
+ctx.fill();
+ctx.strokeStyle = 'black';
+ctx.stroke();
